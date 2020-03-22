6006f0f1c7e70ec48243646825c12ef4f8cd7e47
Artem Trepalin <artemtrepalin@gmail.com>
2020-03-22 22:22:46 +0300

    added try catch

--- src/main/java/GenderDeducer.java
+++ src/main/java/GenderDeducer.java
@@ -2,11 +2,20 @@
 public class GenderDeducer {
 
     public String deduceGender(String name) {
-        String girl = "girl";
-        String boy = "boy";
-        if (name.endsWith("a")) {
-            return girl;
-        } else return boy;
+        try {
+            String girl = "girl";
+            String boy = "boy";
+            if (name == null)
+                throw new IllegalArgumentException("Name cannot be null");
+            if (name.isEmpty())
+                throw new IllegalArgumentException("Name cannot be blank");
+            if (name.endsWith("a")) {
+                return girl;
+            } else return boy;
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        return name;
     }
 }
 
