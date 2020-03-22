a54adfa3c42e599544be272c2a00bc1abecbca5f
Artem Trepalin <artemtrepalin@gmail.com>
2020-03-22 23:19:26 +0300

    error commit
    I don 't know what you want from me. And I don 't know how to write the code correctly.

--- src/main/java/GenderDeducer.java
+++ src/main/java/GenderDeducer.java
@@ -2,20 +2,21 @@
 public class GenderDeducer {
 
     public String deduceGender(String name) {
-        try {
             String girl = "girl";
             String boy = "boy";
-            if (name == null)
-                throw new IllegalArgumentException("Name cannot be null");
-            if (name.isEmpty())
-                throw new IllegalArgumentException("Name cannot be blank");
             if (name.endsWith("a")) {
                 return girl;
             } else return boy;
+
+            try {
+                if (name == null)
+                    throw new IllegalArgumentException("Name cannot be null");
+                if (name.isEmpty())
+                    throw new IllegalArgumentException("Name cannot be blank");
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
         }
-        return name;
     }
 }
+
 
