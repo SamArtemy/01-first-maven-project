0257da31baf18b16b7a3543a63c85537be8a4a52
Artem Trepalin <artemtrepalin@gmail.com>
2020-03-25 19:29:33 +0300

    add test AppTest

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,6 @@ public class App {
     public static void main(String[] args) {
         GenderDeducer genderDeducer = new GenderDeducer();
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter a name");
         try {
             String name = in.next();
             System.out.println("You're my first " + genderDeducer.deduceGender(name) + "friend!");
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,19 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.*;
+import java.util.List;
+
+public class AppTest extends App{
+
+    @Test
+    public void testMain() throws IllegalArgumentException {
+        InputStream is = new ByteArrayInputStream("Artem".getBytes());
+        OutputStream os = new ByteArrayOutputStream();
+        System.setIn(is);
+        System.setOut(new PrintStream(os));
+        App.main(null);
+        os.toString();
+        Assert.assertEquals(os.toString(), "You're my first boyfriend!\n");
+    }
+}
