690e52aae96458e11bc42ab9e2ed694e15fc92bd
Artem Trepalin <artemtrepalin@gmail.com>
2020-03-31 22:35:46 +0300

    test commit

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,52 +1,67 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.awt.print.Book;
 import java.io.*;
-import java.util.GregorianCalendar;
 import java.util.NoSuchElementException;
 
 public class AppTest extends App {
 
     @Test
-    public void testMain() {
-        InputStream is = new ByteArrayInputStream("Artem".getBytes());
+    public void testMain_ArtemInput() {
+        testMain("Artem", "Enter a name\nYou're my first boyfriend!", null);
+    }
+
+    @Test
+    public void testMain_MashaInput() {
+        testMain("Masha", "Enter a name\nYou're my first girlfriend!", null);
+    }
+
+    @Test
+    public void testMain_MikhailInput() {
+        testMain("Mikhail", "Enter a name\nYou're my first boyfriend!", null);
+    }
+
+    @Test
+    public void testMain_TatianaInput() {
+        testMain("Tatiana", "Enter a name\nYou're my first girlfriend!", null);
+    }
+
+    @Test
+    public void testMain_123Input() {
+        testMain("123", "Enter a name\nYou're my first boyfriend!", null);
+    }
+
+    @Test
+    public void testMain_1dot2dot3Input() {
+        testMain("1.2.3", "Enter a name\nYou're my first boyfriend!", null);
+    }
+
+    @Test
+    public void testMain_negative15Input() {
+        testMain("-15", "Enter a name\nYou're my first boyfriend!", null);
+    }
+
+    @Test
+    public void testMain_ZeroInput() {
+        testMain("0", "Enter a name\nYou're my first boyfriend!", null);
+    }
+    
+    @Test
+    public void testMain_EmptyInput() {
+        testMain("", null, NoSuchElementException.class);
+    }
+
+    private void testMain(String input, String expectedOutput, Class<? extends Exception> expectedException) {
+        InputStream is = new ByteArrayInputStream(input.getBytes());
         OutputStream os = new ByteArrayOutputStream();
         System.setIn(is);
         System.setOut(new PrintStream(os));
-        App.main(null);
-        Assert.assertEquals(os.toString(), "Enter a name\nYou're my first boyfriend!");
+        if (expectedException != null) {
+            Assert.expectThrows(expectedException, () -> App.main(null));
+        } else {
+            App.main(null);
+            Assert.assertEquals(os.toString(), expectedOutput);
+        }
     }
 
-    @Test(expectedExceptions = {NoSuchElementException.class})
-    public void testMainEmpty() {
-        InputStream is = new ByteArrayInputStream("".getBytes());
-        OutputStream os = new ByteArrayOutputStream();
-        System.setIn(is);
-        System.setOut(new PrintStream(os));
-        App.main(null);
-
-    }
-
-    @Test(expectedExceptions = {NullPointerException.class})
-    public void testMainNull() {
-        InputStream is = new ByteArrayInputStream(null);
-        OutputStream os = new ByteArrayOutputStream();
-        System.setIn(is);
-        System.setOut(new PrintStream(os));
-        App.main(null);
-    }
-
-    @Test()
-    public void testMainArray() {
-        byte[] array = new byte[]{1, 3, 5, 7};
-        InputStream is = new ByteArrayInputStream(array, 1, 1);
-        OutputStream os = new ByteArrayOutputStream();
-        System.setIn(is);
-        System.setOut(new PrintStream(os));
-        App.main(null);
-        Assert.assertEquals(os.toString(), "Enter a name\nYou're my first boyfriend!");
-
-
-    }
 }
