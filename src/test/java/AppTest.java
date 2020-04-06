2841441f27cbe6ee68dc8a8348246ad600f39a54
Artem Trepalin <artemtrepalin@gmail.com>
2020-04-06 23:44:47 +0300

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -9,10 +9,14 @@ public class AppTest extends App {
 
     @DataProvider
     public static Object[][] charsAndSymbols() {
-        return new Object[][]{{"123", "Enter a name\nYou're my first boyfriend!"},
-                {"1.2.3","Enter a name\nYou're my first boyfriend!"}, {"-15","Enter a name\nYou're my first boyfriend!"},
-                {"0", "Enter a name\nYou're my first boyfriend!"},{"Artem","Enter a name\nYou're my first boyfriend!"},
-                {"Mikhail","Enter a name\nYou're my first boyfriend!"},{"Masha", "Enter a name\nYou're my first girlfriend!"},
+        return new Object[][]{
+                {"123", "Enter a name\nYou're my first boyfriend!"},
+                {"1.2.3", "Enter a name\nYou're my first boyfriend!"},
+                {"-15", "Enter a name\nYou're my first boyfriend!"},
+                {"0", "Enter a name\nYou're my first boyfriend!"},
+                {"Artem", "Enter a name\nYou're my first boyfriend!"},
+                {"Mikhail", "Enter a name\nYou're my first boyfriend!"},
+                {"Masha", "Enter a name\nYou're my first girlfriend!"},
                 {"Tatiana", "Enter a name\nYou're my first girlfriend!"}};
     }
 
@@ -20,28 +24,32 @@ public class AppTest extends App {
     public static Object[][] emptySymbol() {
         return new Object[][]{{"", null}};
     }
-    
+
     @Test(dataProvider = "charsAndSymbols")
     public void testMain_charsAndSymbolsInput(String expectedText, String expectedOutput) {
-        testMain(expectedText, expectedOutput, null);
+        testMain(expectedText, expectedOutput);
     }
 
     @Test(dataProvider = "emptySymbol")
-    public void testMain_EmptyInput(String expectedText, String expectedOutput) {
-        testMain(expectedText, expectedOutput, NoSuchElementException.class);
+    public void testMain_EmptyInput(String expectedText) {
+        testMain(expectedText, NoSuchElementException.class);
     }
 
-    private void testMain(String input, String expectedOutput, Class<? extends Exception> expectedException) {
+    private void testMain(String input, Class<? extends Exception> expectedException) {
         InputStream is = new ByteArrayInputStream(input.getBytes());
         OutputStream os = new ByteArrayOutputStream();
         System.setIn(is);
         System.setOut(new PrintStream(os));
-        if (expectedException != null) {
-            Assert.expectThrows(expectedException, () -> App.main(null));
-        } else {
-            App.main(null);
-            Assert.assertEquals(os.toString(), expectedOutput);
-        }
+        Assert.expectThrows(expectedException, () -> App.main(null));
     }
+
+    private void testMain(String input, String expectedOutput) {
+        InputStream is = new ByteArrayInputStream(input.getBytes());
+        OutputStream os = new ByteArrayOutputStream();
+        System.setIn(is);
+        System.setOut(new PrintStream(os));
+        App.main(null);
+        Assert.assertEquals(os.toString(), expectedOutput);
+        }
 
 }
